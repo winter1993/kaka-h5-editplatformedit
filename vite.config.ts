7fd1cd68e8989c83c8b0a4ba05baa87b54b6ad99
iswinter1993 <iswinter@aliyun.com>
2023-03-03 12:25:11 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src')
         }
     },
+    base:'/kaka-h5-editplatformedit/',
     plugins: [vue()],
     server: {
         port: 8082, //启动端口
