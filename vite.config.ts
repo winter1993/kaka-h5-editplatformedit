82cf1a3e817082a347d83450645998fc085a49f9
iswinter1993 <iswinter@aliyun.com>
2023-03-03 13:19:34 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     build:{
-        outDir:'docs'
+        outDir:'docs',
     },
     resolve: {
         //设置别名
