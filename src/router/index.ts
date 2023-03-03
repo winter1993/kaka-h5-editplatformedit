94631db1c009e9f205b05d14e9f953927f2ea00b
iswinter1993 <iswinter@aliyun.com>
2023-03-03 15:59:42 +0800

    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,8 +33,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    // history:createWebHistory(),
-    history:createWebHashHistory(import.meta.env.BASE_URL), //前端打包使用createWebHashHistory
+    history:createWebHistory(import.meta.env.BASE_URL),
+    // history:createWebHashHistory(import.meta.env.BASE_URL), //前端打包使用createWebHashHistory
     routes
 })
 
